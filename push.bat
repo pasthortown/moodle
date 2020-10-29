004abb0462ce6790808c49408cb37487a83e8779
luis.salazar@kfc.com.ec <luis.salazar@kfc.com.ec>
2020-10-28 21:20:36 -0500

    mié__28_10_2020

--- push.bat
+++ push.bat
@@ -1,7 +1,9 @@
-set datestr=%date%
-set result=%datestr:/=-%
-@echo %result%
+set _my_datetime=%date%
+set _my_datetime=%_my_datetime: =_%
+set _my_datetime=%_my_datetime::=%
+set _my_datetime=%_my_datetime:/=_%
+set _my_datetime=%_my_datetime:.=_%
 
 git add .
-git commit -m hola
+git commit -m %_my_datetime%
 git push -u origin master
